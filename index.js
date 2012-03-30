35c479a68b27527d1c8cc2776e9b87a51aa9c354
Mark Trostler <mark@zzo.com>
2012-03-30 07:02:03 -0700

    bump to sanity

---
+++ index.js
@@ -0,0 +1,168 @@
+var http = require('http')
+    , webdriverjs = require("webdriverjs")
+    ;
+
+function Proxy(conf) {
+    this.host = (conf && conf.host) || 'localhost';
+    this.port = (conf && conf.port) || 8080;
+    this.selHost = (conf && conf.selHost) || 'localhost';
+    this.selPort = (conf && conf.selPort) || 4444;
+}
+
+Proxy.prototype = {
+    selFunc:  function(proxy, url, cb) {
+        var browser = webdriverjs.remote({
+            host: this.selHost
+            , port: this.selPort
+            , desiredCapabilities: { browserName: 'firefox', seleniumProtocol: 'WebDriver', proxy: { httpProxy: proxy } }
+        });
+
+        browser
+            .init()
+            .url(url)
+            .end(cb);
+    },
+
+    selHAR:  function(name, selCB, cb) {
+        var _this = this;
+        this.start(function(err, data) {
+            if (!err) {
+                _this.startHAR(data.port, name, function(err, resp) {
+                    if (!err) {
+                        selCB(_this.host +  data.port, function () {
+                            _this.getHAR(data.port, function(err, resp) {
+                                _this.stop(data.port, function() {
+                                    if (err) {
+                                        cb(err);
+                                    } else {
+                                        cb(null, resp);
+                                    }
+                                });
+                            });
+                        });
+                    } else {
+                        _this.stop(data.port, function() { cb(err); });
+                    }
+                });
+            } else {
+                cb(err);
+            }
+         });
+    },
+
+    start: function(port, cb) {
+        var postData = '';
+        if (!cb) {
+            cb = port;
+        }
+        if (typeof(port) === 'Number') {
+            postData = 'port=' + port;
+        }
+
+        this.doReq('POST', '/proxy', postData, function(err, data) {
+            var obj;
+            if (!err) {
+                obj = JSON.parse(data);
+                cb(null, obj);
+            } else {
+                cb(err);
+            }
+        });
+    },
+
+    stop: function(port, cb) {
+        this.doReq('DELETE', '/proxy/' + port, cb);
+    },
+
+    getHAR: function(port, cb) {
+        this.doReq('GET', '/proxy/' + port + '/har', cb);
+    },
+
+    startHAR: function(port, name, cb) {
+        if (!cb) {
+            cb = name;
+            name = 'Page 1';
+        }
+        this.doReq('PUT', '/proxy/' + port + '/har', 'initialPageRef=' + name, cb);
+    },
+
+    newPage: function(port, name, cb) {
+        this.doReq('PUT', '/proxy/' + port + '/har', 'pageRef=' + name, cb);
+    },
+
+    /*
+     *  downstreamKbps - Sets the downstream kbps
+     *  upstreamKbps - Sets the upstream kbps
+     *  latency - Add the given latency to each HTTP request
+     */
+    limit: function(port, obj, cb) {
+        var data = '';
+        for (key in obj) {
+            data += key + '=' + obj[key] + '&';
+        }
+        this.doReq('PUT', '/proxy/' + port + '/limit', data, cb);
+    },
+
+    doHAR: function(url, cb) {
+        var _this = this;
+        this.start(function(err, data) {
+            if (!err) {
+                _this.startHAR(data.port, url, function(err, resp) {
+                    if (!err) {
+                        _this.selFunc(_this.host +  data.port, url, function () {
+                            _this.getHAR(data.port, function(err, resp) {
+                                _this.stop(data.port, function() {
+                                    if (err) {
+                                        cb(err);
+                                    } else {
+                                        cb(null, resp);
+                                    }
+                                });
+                            });
+                        });
+                    } else {
+                        _this.stop(data.port, function() { cb(err); });
+                    }
+                });
+            } else {
+                cb(err);
+            }
+         });
+    },
+
+    doReq: function(method, url, data, cb) {
+
+        if (!cb) { // for empty requests
+            cb = data;
+        }
+
+        var options = {
+            host: this.host
+            , port: this.port
+            , method: method
+            , path: url
+        }
+        , req = http.request(options, function(res) {
+            var data = '';
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', function() {
+                cb(null, data);
+            });
+        })
+        ;
+
+        req.on('error', cb);
+
+        if (typeof(data) === 'String') {
+            req.write(data);
+        }
+        req.end();
+    }
+};
+
+module.exports = {
+    Proxy: Proxy
+};
